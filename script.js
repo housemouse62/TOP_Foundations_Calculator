70743c313b24d8067e97e623e9c759d44689f519
housemouse62 <ryan.skeels@gmail.com>
2025-08-30 23:12:59 +0200

    basic math operation formulas functional

---
+++ script.js
@@ -0,0 +1,32 @@
+const add = function(x, y) {
+    console.log(y)
+    let sum = (x + y);
+    return sum
+};
+
+// let value = add(1, 2)
+// console.log(value)
+
+const multiply = function(x, y) {
+    let multiply = (x * y);
+    return multiply
+};
+
+// let value = multiply(8, 2)
+// console.log(value)
+
+const divide = function(x, y) {
+    let divide = (x / y);
+    return divide
+};
+
+// let value = divide(8, 2)
+// console.log(value)
+
+const subtract = function(x, y) {
+    let subtract = (x - y);
+    return subtract
+};
+
+let value = subtract(8, 2)
+console.log(value)
