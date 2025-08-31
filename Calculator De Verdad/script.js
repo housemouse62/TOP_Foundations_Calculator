9b978843784fee801743318367c705dc2a7f7b52
housemouse62 <ryan.skeels@gmail.com>
2025-08-31 19:22:48 +0200

    Added box shadows and movement to buttons. Added eventListeners to buttons.
    Need to have button presses built multi-digit
    numbers on the display and then save them as a
    variable when an operator is pressed.

--- Calculator De Verdad/script.js
+++ Calculator De Verdad/script.js
@@ -1,1 +1,41 @@
-const calcBody = document.querySelector("#container")
+const calcBody = document.querySelector("#container");
+const display = document.querySelector("#display");
+const one = document.querySelector("#one");
+let firstNumber;
+
+
+one.addEventListener('click', () => {
+    display.textContent = '1';
+});
+
+two.addEventListener('click', () => {
+    display.textContent = '2';
+});
+
+three.addEventListener('click', () => {
+    display.textContent = '3';
+});
+
+four.addEventListener('click', () => {
+    display.textContent = '4';
+});
+
+five.addEventListener('click', () => {
+    display.textContent = '5';
+});
+
+six.addEventListener('click', () => {
+    display.textContent = '6';
+});
+
+seven.addEventListener('click', () => {
+    display.textContent = '7';
+});
+
+eight.addEventListener('click', () => {
+    display.textContent = '8';
+});
+
+nine.addEventListener('click', () => {
+    display.textContent = '9';
+});
