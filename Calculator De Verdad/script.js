8deee530676c7fc7f82778dde70492c6ed05f1d3
housemouse62 <ryan.skeels@gmail.com>
2025-08-31 23:53:02 +0200

    Rounding function added. Display function added.
    Added a separate rounding function to deal with
    all situations.

--- Calculator De Verdad/script.js
+++ Calculator De Verdad/script.js
@@ -1,33 +1,47 @@
-
-
 const calcBody = document.querySelector("#container");
 const display = document.querySelector("#display");
 const numberButtons = document.querySelectorAll(".number")
 const operatorButtons = document.querySelectorAll(".operator")
 const equalsButton = document.querySelector("#equals")
 const clearDisplay = document.querySelector("#C")
+const clearCurrent = document.querySelector("#CE")
 
-console.log(operatorButtons)
 let firstNumber = '';
 let secondNumber = '';
 let presentNumber = '';
 let operatingOperator = '';
-let sum;
+let sumDisplay;
+
+//current display
+function currentDisplay(value) {
+    display.textContent = value;
+}
+
+// Rounding Function
+function roundResult(nmbr) {
+    console.log({nmbr})
+    return roundedNumber = Math.round((nmbr * 1000000000)/1000000000)
+}
 
 //number button entry logic
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', () => {
+        if (presentNumber.length <= 10) {
         presentNumber += numberButton.textContent;
-        display.textContent = presentNumber;
+        } else {
+            presentNumber;
+        }
+        currentDisplay(presentNumber);
     });
 });
+
 
 // stores first number entered when operator is pressed
 operatorButtons.forEach(operatorButton => {
     operatorButton.addEventListener('click', () => {
         firstNumber = Number(presentNumber);
         operatingOperator = operatorButton.textContent;
-        display.textContent = operatingOperator;
+        currentDisplay(operatingOperator);
         presentNumber = '';
     });
 });
@@ -35,34 +49,43 @@ operatorButtons.forEach(operatorButton => {
 //equals button
 equalsButton.addEventListener('click', () => {
     secondNumber = Number(presentNumber);
-    operate(firstNumber, operatingOperator, secondNumber);
-    display.textContent = sum;
+    let result = operate(firstNumber, operatingOperator, secondNumber);
+    console.log(result)
+    let roundedResult = roundResult(result);
+    currentDisplay(roundedResult);
+
+    presentNumber = roundedResult;
+    firstNumber = '';
+    secondNumber = '';
+    operatingOperator = '';
 });
 
 // operation function
 function operate(numOne, operator, numTwo) {
     if (operator === '+') {
-        add(numOne, numTwo)
+        return add(numOne, numTwo)
     } else if (operator === '-') {
-        subtract(numOne, numTwo)
+        return subtract(numOne, numTwo)
     } else if (operator === 'X') {
-        multiply(numOne, numTwo)
+        return multiply(numOne, numTwo)
     } else {
-        divide(numOne, numTwo)
-    };
-    display.textContent = sum;
-    presentNumber = '';
-    firstNumber = '';
-    secondNumber = '';
-    operatingOperator = '';
+        return divide(numOne, numTwo)
+    }
 };
 
+//clear math & display
 clearDisplay.addEventListener('click', () => {
     presentNumber = '';
     firstNumber = '';
     secondNumber = '';
     operatingOperator = '';
-    display.textContent = '0';
+    currentDisplay('0');
+})
+
+//clear current number
+clearCurrent.addEventListener('click', () => {
+    presentNumber = '';
+    currentDisplay('0');
 })
 
 // basic operation functions
